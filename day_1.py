6fa9f9e75e44cd0b5c42c1bc646906a3f9c9a2cd
misdk <mishasdk@gmail.com>
2022-12-02 11:18:15 +0200

    day 1

---
+++ day_1.py
@@ -0,0 +1,20 @@
+input_name = 'input.txt'
+
+with open(input_name) as file_input:
+    lines = [line.strip() for line in file_input.readlines()]
+data = [[]]
+for line in lines:
+    if line == "":
+        data.append([])
+    else:
+        data[len(data) - 1].append(int(line))
+
+sums = list(map(sum, data))
+
+# part1
+most = max(sums)
+print(most)
+
+# part2
+most_3 = sum(sorted(sums)[-3:])
+print(most_3)
